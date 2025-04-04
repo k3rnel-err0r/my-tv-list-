529601e7ac9126f17748e235a7451389299e47cd
Antonio Felizzola <antonio.qfel@gmail.com>
2025-04-04 13:42:42 -0400

    Fixed problem with landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from './context/ThemeContext'
 import Home from './components/Home'
 import List from './components/List'
